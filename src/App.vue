30a47d84d1d55fa1ab9f21977add1b5e3e482a89
Janmichaelagapay21 <janmichaelagapay21@gmail.com>
2026-01-13 19:27:07 +0800

    Added porfolio for hosting

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+ 	<NavBar/>
+ 	<div class="container-fluid">
+ 		<About/>
+ 		<Projects/>
+ 		<Tools/>
+ 		<Contact/>
+ 		
+ 	</div>
+ 	<Footer/>
+
+</template>
+
+<script setup>
+	import NavBar from './components/NavBar.vue'
+	import About from './components/About.vue'
+	import Projects from './components/Projects.vue'
+	import Tools from './components/Tools.vue'
+	import Contact from './components/Contact.vue'
+	import Footer from './components/Footer.vue'
+</script>
